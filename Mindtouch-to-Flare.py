288661afcfb8ffb43c4c0aefad7a05e58e15dfbe
ProgramZeta <pz@programzeta.com>
2011-12-19 13:05:42 -0500

    streamlined error-checking

--- Mindtouch-to-Flare.py
+++ Mindtouch-to-Flare.py
@@ -102,13 +102,10 @@ def verify_url(url):
         print "Successfully accessed " + mindtouch_api_url
     except urllib2.URLError, e:
         print e
-        raise
     except urllib2.HTTPError, e:
         print e
-        raise
     except:
         print "Unknown error accessing " + mindtouch_api_url
-        raise
     return url
 
 # Make sure we can place folders and files in the given directory
@@ -130,10 +127,8 @@ def verify_directory(directory):
                 os.access(directory, os.W_OK)
             except os.error, e:
                 print e
-                raise
             except:
                 print "Unknown error occurred when trying to create " + directory
-                raise
             else:
                 return directory
         else:
@@ -186,13 +181,14 @@ def link_path_generator(link_url, page_url):
     return link_path
 
 # Program entry point
+
 args = argument_parser.parse_args()
-print "Mindtouch Wiki to Flare Project Conversion Tool"
 url = args.url
 directory = args.output 
 username = args.username
 password = args.password
 verbose = args.verbose
+print "Mindtouch Wiki to Flare Project Conversion Tool"
 if args.interactive == True:
     try:
         results = interactive_mode()
@@ -234,139 +230,135 @@ except Exception, e:
     print "Error when accessing Mindtouch wiki page list at " + page_listing_url
     print e
 
-if True:
-    page_url = None
-    page_title = None
-    page_path = None
-    page_contents = None
-    for event, element in etree.iterparse(page_listing, events=("start", "end")):
-        if element.tag == "page" and event == "start":
-            if "href" in element.attrib:
-                page_title = None
-                page_path = None
-                page_url = unicode(element.attrib["href"], "ascii") 
-                page_url = page_url.split('?redirects')[0] + "/contents"
-        if element.tag == "title" and event == "end":
-            page_title = element.text
-        if element.tag == "path" and event == "end":
-            try:
-                page_path = element.text
-                # A double-forward slash "//" is an escaped single slash; we need to convert those first
-                page_path = page_path.replace("//", "%2F%2F")
-                # If we can't find a slash, we're in the root directory
-                if page_path.find('/') == -1:
-                    page_path = ""
-                else:
-                    # We don't want the last bit after the slash as that's the file name
-                    page_path = page_path.rsplit('/', 1)[0] + "/"
-            except:
-                # If we've excepted, then we're not dealing with a text string for the path
-                # This is usually a self-closing tag in the XML, indicative of a root topic 
-                page_path = ""
-            # Make page-path
-            
-        if page_url != None and page_title != None and page_path != None:
-            file_path = directory + url.split('http://')[1] + page_path
-            full_file_path = file_path + page_title.replace("/", " ") + ".htm"
-            print "Creating topic file " + full_file_path.encode('utf-8')
-            try:
-                os.makedirs(file_path.encode('utf-8'))
-                if verbose:
-                    print "Created directory " + file_path.encode('utf-8')
-            except:
-                if verbose:
-                    print "Directory " + file_path.encode('utf-8') + " already exists"
-            try:
-                # Create a new topic and open it for writing 
-                f = open(full_file_path.encode('utf-8'), 'w')
-            except:
-                print "Error creating file " + full_file_path.encode('utf-8')
-                raise
-            if verbose:
-                print "Accessing " + page_url.encode('utf-8')
-            try:
-                # Access the page URL
-                page_content_listing = urllib2.urlopen(page_url.encode('utf-8'))
-                try:
-                    for inner_event, inner_element in etree.iterparse(page_content_listing):
-                        # Get the contents of the body tag, unless it has the "toc" attribute
-                        if not inner_element.attrib:
-                            if inner_element.tag == "body":
-                                if verbose:
-                                    print "Parsing HTML contents"
-                                # parse through the html contents
-                                page_contents_tree = lxml.html.fragment_fromstring(inner_element.text, create_parent=True)
-                                context = etree.iterwalk(page_contents_tree)
-                                for action, elem in context:
-                                    if elem.tag == "a" and "href" in elem.attrib:
-                                        # fix links
-                                        if verbose:
-                                            print "Patching link to " + elem.attrib["href"]
-                                        elem.attrib["href"] = link_path_generator(elem.attrib["href"], page_url)
-                                    if elem.tag == "img" and "src" in elem.attrib:
-                                        full_image_path = None
-                                        image_url = elem.attrib["src"].split('?')[0]
-                                        if image_url.find(url) != -1:
-                                            image_path = image_url.split(url)[1].rsplit('/', 1)[0] + "/"
-                                            image_file_path = directory + url.split('http://')[1] + image_path
-                                            image_file_name = image_url.split(url)[1].rsplit('/', 1)[1]
-                                            if verbose:
-                                                print "Downloading image file " + image_url
-                                            try:
-                                                image_urlobject = urllib2.urlopen(image_url)
-                                                image_object = image_urlobject.read()
-                                            except urllib2.URLError, e:
-                                                print "Error downloading image file " + image_url
-                                            except:
-                                                print "Error downloading image file " + image_url
-                                                raise
-                                            try:
-                                                os.makedirs(image_file_path.encode('utf-8'))
-                                            except:
-                                                if verbose:
-                                                    print "Path " + image_file_path.encode('utf-8') + " already exists"
-                                            else:
-                                                if verbose:
-                                                    print "Created path " + image_file_path.encode('utf-8')
-                                            try:
-                                                full_image_path = image_file_path + image_file_name
-                                                if verbose:
-                                                    print "Writing image file " + full_image_path.encode('utf-8')
-                                                image_file = open(full_image_path.encode('utf-8'), 'wb')
-                                                image_file.write(image_object)
-                                            except:
-                                                print "Error writing local file " + full_image_path
-                                                raise
-                                            if verbose:
-                                                print "Patching image link in " + full_file_path
-                                            elem.attrib["src"] = link_path_generator(elem.attrib["src"], page_url)
-                                        
-                except:
-                    print "Error parsing contents of " + page_url.encode('utf-8')
-                    raise
-                page_contents = lxml.html.tostring(page_contents_tree)
-            except:
-                print "Error accessing " + page_url.encode('utf-8')
-                raise
-            try:
-                # Write the starting html content to the topic
-                f.write("<html><head><title>" + page_title.encode('utf-8') + "</title></head><body>")
-                # Insert an h1 element with the page title
-                f.write("<h1>" + page_title.encode('utf-8') + "</h1>")
-                # Insert the page contents
-                f.write(page_contents.encode('utf-8'))
-                # Write the closing tags
-                f.write("</body></html>")
-            except:
-                print "Error writing page contents to file"
-                raise
-            finally:
-                f.close()
-            print "Finished writing file " + full_file_path.encode('utf-8')
-            print " "
-            page_url = None
+page_url = None
+page_title = None
+page_path = None
+page_contents = None
+for event, element in etree.iterparse(page_listing, events=("start", "end")):
+    if element.tag == "page" and event == "start":
+        if "href" in element.attrib:
             page_title = None
             page_path = None
-            file_path = None
-            # Sleep so we do not over-saturate the server
-            time.sleep(3)
+            page_url = unicode(element.attrib["href"], "ascii") 
+            page_url = page_url.split('?redirects')[0] + "/contents"
+    if element.tag == "title" and event == "end":
+        page_title = element.text
+    if element.tag == "path" and event == "end":
+        try:
+            page_path = element.text
+            # A double-forward slash "//" is an escaped single slash; we need to convert those first
+            page_path = page_path.replace("//", "%2F%2F")
+            # If we can't find a slash, we're in the root directory
+            if page_path.find('/') == -1:
+                page_path = ""
+            else:
+                # We don't want the last bit after the slash as that's the file name
+                page_path = page_path.rsplit('/', 1)[0] + "/"
+        except:
+            # If we've excepted, then we're not dealing with a text string for the path
+            # This is usually a self-closing tag in the XML, indicative of a root topic 
+            page_path = ""
+        # Make page-path
+        
+    if page_url != None and page_title != None and page_path != None:
+        file_path = directory + url.split('http://')[1] + page_path
+        full_file_path = file_path + page_title.replace("/", " ") + ".htm"
+        print "Creating topic file " + full_file_path.encode('utf-8')
+        try:
+            os.makedirs(file_path.encode('utf-8'))
+            if verbose:
+                print "Created directory " + file_path.encode('utf-8')
+        except:
+            if verbose:
+                print "Directory " + file_path.encode('utf-8') + " already exists"
+        try:
+            # Create a new topic and open it for writing 
+            f = open(full_file_path.encode('utf-8'), 'w')
+        except:
+            print "Error creating file " + full_file_path.encode('utf-8')
+        if verbose:
+            print "Accessing " + page_url.encode('utf-8')
+        try:
+            # Access the page URL
+            page_content_listing = urllib2.urlopen(page_url.encode('utf-8'))
+            try:
+                for inner_event, inner_element in etree.iterparse(page_content_listing):
+                    # Get the contents of the body tag, unless it has the "toc" attribute
+                    if not inner_element.attrib:
+                        if inner_element.tag == "body":
+                            if verbose:
+                                print "Parsing HTML contents"
+                            # parse through the html contents
+                            page_contents_tree = lxml.html.fragment_fromstring(inner_element.text, create_parent=True)
+                            context = etree.iterwalk(page_contents_tree)
+                            for action, elem in context:
+                                if elem.tag == "a" and "href" in elem.attrib:
+                                    # fix links
+                                    if verbose:
+                                        print "Patching link to " + elem.attrib["href"]
+                                    elem.attrib["href"] = link_path_generator(elem.attrib["href"], page_url)
+                                if elem.tag == "img" and "src" in elem.attrib:
+                                    full_image_path = None
+                                    image_url = elem.attrib["src"].split('?')[0]
+                                    if image_url.find(url) != -1:
+                                        image_path = image_url.split(url)[1].rsplit('/', 1)[0] + "/"
+                                        image_file_path = directory + url.split('http://')[1] + image_path
+                                        image_file_name = image_url.split(url)[1].rsplit('/', 1)[1]
+                                        if verbose:
+                                            print "Downloading image file " + image_url
+                                        try:
+                                            image_urlobject = urllib2.urlopen(image_url)
+                                            image_object = image_urlobject.read()
+                                        except urllib2.URLError, e:
+                                            print "Error downloading image file " + image_url
+                                            
+                                        except:
+                                            print "Error downloading image file " + image_url
+                                        try:
+                                            os.makedirs(image_file_path.encode('utf-8'))
+                                        except:
+                                            if verbose:
+                                                print "Path " + image_file_path.encode('utf-8') + " already exists"
+                                        else:
+                                            if verbose:
+                                                print "Created path " + image_file_path.encode('utf-8')
+                                        try:
+                                            full_image_path = image_file_path + image_file_name
+                                            if verbose:
+                                                print "Writing image file " + full_image_path.encode('utf-8')
+                                            image_file = open(full_image_path.encode('utf-8'), 'wb')
+                                            image_file.write(image_object)
+                                        except:
+                                            print "Error writing local file " + full_image_path
+                                        if verbose:
+                                            print "Patching image link in " + full_file_path
+                                        elem.attrib["src"] = link_path_generator(elem.attrib["src"], page_url)
+                                    
+            except:
+                print "Error parsing contents of " + page_url.encode('utf-8')
+            page_contents = lxml.html.tostring(page_contents_tree)
+        except:
+            print "Error accessing " + page_url.encode('utf-8')
+            raise
+        try:
+            # Write the starting html content to the topic
+            f.write("<html><head><title>" + page_title.encode('utf-8') + "</title></head><body>")
+            # Insert an h1 element with the page title
+            f.write("<h1>" + page_title.encode('utf-8') + "</h1>")
+            # Insert the page contents
+            f.write(page_contents.encode('utf-8'))
+            # Write the closing tags
+            f.write("</body></html>")
+        except:
+            print "Error writing page contents to file"
+            raise
+        finally:
+            f.close()
+        print "Finished writing file " + full_file_path.encode('utf-8')
+        print " "
+        page_url = None
+        page_title = None
+        page_path = None
+        file_path = None
+        # Sleep so we do not over-saturate the server
+        time.sleep(3)
